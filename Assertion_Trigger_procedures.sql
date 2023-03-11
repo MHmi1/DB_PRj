eb14ee2e89319c02d3efa24d0e3952ed2ac1bda4
Hami <78771728+MHmi1@users.noreply.github.com>
2023-03-11 17:30:15 +0330

    3 triggers added to project

--- Assertion_Trigger_procedures.sql
+++ Assertion_Trigger_procedures.sql
@@ -1,4 +1,6 @@
+--TRIGGER SECTION 
 
+-- This section sends a notification email if the user's password is updated in the system 
 CREATE TRIGGER Pass_Change_Warning 
 ON "User"  
 AFTER  UPDATE  
@@ -10,4 +12,33 @@ if update(password)
         @body = 'Hi , Your password has been changed. If this was not done by you, please let us know so that we can check the security of your account again . WOODENDOOR inc .',  
         @subject = 'Password Reminder';  
 GO
+
+
+-- This section saves users' information entered in the system, including email and date of entry
+ CREATE TRIGGER New_User_Audit   
+    ON "User"  
+    FOR INSERT  
+    AS  
+    BEGIN  
+      Declare @Id int  
+      SELECT @Id = email from inserted  
+      INSERT INTO User_Audit 
+      VALUES ('New User with Id = ' + CAST(@Id AS VARCHAR(50)) + ' is added at ' + CAST(Getdate() AS VARCHAR(22)))  
+    END  
+	go 
+
+
+-- This section saves users' information DELETED from the system, including email and date of deletion
+
+	    CREATE TRIGGER Del_User_Audit     
+    ON "User"  
+    FOR DELETE  
+    AS  
+    BEGIN  
+      Declare @Id1 int  
+      SELECT @Id1 = email from deleted  
+      INSERT INTO User_Audit  
+      VALUES ('An existing User with Id = ' + CAST(@Id1 AS VARCHAR(50)) + ' is deleted at ' + CAST(Getdate() AS VARCHAR(22)))  
+    END 
+	go 
 
