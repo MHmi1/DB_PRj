4f88bd23baca1b459bb49148d90cc24fbf10deb1
Ali Hemmati <87714776+NeverSayNeveriinon@users.noreply.github.com>
2023-03-16 03:57:18 +0330

    Queries (1 to 13) "SQLs"

--- Req_Queries.sql
+++ Req_Queries.sql
@@ -22,13 +22,13 @@ FROM Ads
 WHERE Aid IN (SELECT Ads_id
               FROM Apply_Ads  
               GROUP BY Ads_id 
-              HAVING Count(*) > 20);
+              HAVING Count(*) > 1); -- 1 instead of 20
 				
 
 
 -- Query4 --
 SELECT Business.BName AS Business_Name
-FROM (  (Business JOIN Ads ON Business.BName = Ads.BName) JOIN Need_Skill ON Aid = Ads_id ) 
+FROM ( (Business JOIN Ads ON Business.BName = Ads.BName) JOIN Need_Skill ON Aid = Ads_id ) 
 WHERE Need_Skill.Skill_Name Like '%Python%';
 
 
@@ -38,20 +38,20 @@ SELECT Email AS Applicant_Email
 FROM (Applicant JOIN Apply_Ads ON Email = App_Email)
 WHERE Status = N'Rejected' 
 GROUP BY Email
-HAVING Count(*) > 5;
+HAVING Count(*) > 0; -- 0 instead of 5
 
 
 
 -- Query6 --
-SELECT Avg (DATEDIFF (year, GETDATE(), Birthday) ) AS Avg_Age
-FROM Applicant
+SELECT Avg ( DATEDIFF (Year, Birthday, GETDATE()) ) AS Avg_Age
+FROM Applicant 
 WHERE Email IN (SELECT Email
-		FROM (Applicant JOIN Has_Skill ON Email = App_Email)
-		WHERE Skill_Name LIKE '%Objective-c%'
-		INTERSECT
-		SELECT Email
-		FROM (Applicant JOIN Has_Skill ON Email = App_Email)
-		WHERE Skill_Name LIKE '%Network+%');
+				FROM (Applicant JOIN Has_Skill ON Email = App_Email)
+				WHERE Skill_Name LIKE '%Objective-c%'
+				INTERSECT
+				SELECT Email
+				FROM (Applicant JOIN Has_Skill ON Email = App_Email)
+				WHERE Skill_Name LIKE '%Network+%');
 
 
 
@@ -62,12 +62,11 @@ WHERE (Work_Exp BETWEEN 3 AND 5) AND (Req_Sal BETWEEN 12 AND 16);
 
 
 
--- Query8 -- (need to test maybe*********)
+-- Query8 -- (Uses View)
 SELECT Applicant.* 
 FROM Applicant 
 WHERE Gender ='M' AND Email IN (SELECT App_email
-				FROM (Ads JOIN Apply_Ads ON Aid = Ads_id)
-				WHERE Applicant.AppLocation = Ads.Ads_Location ); 
+								FROM Same_Location);
 
 
 
@@ -75,7 +74,7 @@ WHERE Gender ='M' AND Email IN (SELECT App_email
 SELECT * 
 FROM Ads 
 WHERE Offer_Sal > (SELECT Avg(Req_Sal)  
-                   FROM Applicant );
+                   FROM Applicant);
 
 
 
@@ -83,13 +82,36 @@ WHERE Offer_Sal > (SELECT Avg(Req_Sal)
 SELECT Gender, Avg(Offer_Sal) AS Avg_Offer_Sal 
 FROM ( (Applicant JOIN Apply_Ads ON Email = App_Email) JOIN Ads ON Ads_id = Aid)
 WHERE Apply_Ads.Status = 'Accepted' 
-GROUP BY Gender
+GROUP BY Gender;
 
 
 
--- Query11 -- (need to test maybe*********)
+-- Query11 -- 
 SELECT Post.* 
-FROM ( (Post JOIN Blogger ON Writer_id = Uid) JOIN Applicant ON Blogger.Uid = Applicant.Uid)
-WHERE ( Work_Exp BETWEEN 2 AND 4 ) AND Email IN (SELECT App_Email 
-						 FROM Apply_Ads 
-					   	 WHERE Status ='Rejected')
+FROM (Post JOIN Applicant ON Writer_id = Applicant.Uid)
+WHERE ( Work_Exp BETWEEN 2 AND 10 ) AND Email IN (SELECT App_Email 
+												 FROM Apply_Ads 
+											     WHERE Status ='Rejected');
+
+
+
+-- Query12 -- (Uses View)
+SELECT Count(*) AS No_of_Businesses
+FROM Query12_1 , Query12_2 
+WHERE Avg_OfferSalary > Max_ReqSalary;
+
+
+
+-- Query13 -- (Uses View)
+SELECT Gender , COUNT(*) AS No_of_Applicants
+FROM Applicant
+WHERE Uid IN (SELECT Applicant.Uid
+		      FROM ( (Applicant JOIN Add_Fav ON Applicant.Uid = Add_Fav.Uid) JOIN Most_Popular_Post ON Add_Fav.Pid = Most_Popular_Post.Pid )
+			  GROUP BY Applicant.Uid
+
+			  INTERSECT
+
+			  SELECT Applicant.Uid
+			  FROM (Applicant JOIN Two_Comment ON Applicant.Uid = Two_Comment.Uid)
+			  GROUP BY Applicant.Uid )
+GROUP BY Gender;
